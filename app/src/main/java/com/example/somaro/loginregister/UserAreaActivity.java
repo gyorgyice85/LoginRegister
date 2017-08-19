472c62ac17f88693ef10d0e6d1a984e77b501e97
Somaroo <somarhasan@gmail.com>
2017-08-19 15:45:04 +0200

    new_17.08.2017

--- app/src/main/java/com/example/somaro/loginregister/UserAreaActivity.java
+++ app/src/main/java/com/example/somaro/loginregister/UserAreaActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,12 +16,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UserAreaActivity extends Activity {
-    Button button;
-    ImageView imageView ;
+    public static final int IMAGE_GALLERY_REQUEST = 20;
+    Button btnTakePhoto;
+    private ImageView imageView ;
     static final int CAM_REQUEST = 1;
 
 
@@ -28,55 +36,95 @@ public class UserAreaActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
 
-        button = (Button) findViewById(R.id.button) ;
+        btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto) ;
         imageView = (ImageView) findViewById(R.id.imageView);
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                //File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                //String pictureName = getPictureName();
+                //File imageFile = new File(pictureDirectory, pictureName);
+                // Uri pictureUri = Uri.fromFile(imageFile);
+                //camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+
                 startActivityForResult(camera_intent, CAM_REQUEST);
-
-                File file = getFile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-
-
 
 
             }
+            private String getPictureName(){
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                String timestamp = sdf.format(new Date());
+                return "PlantPlacesImage" + timestamp + ".jpg";
+            }
+
+
         });
 
-        final EditText etUsename = (EditText) findViewById(R.id.etUsername);
-        final EditText etAge = (EditText) findViewById(R.id.etAge);
+
+        //final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        //final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView welcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         String username = intent.getStringExtra("username");
-        int age = intent.getIntExtra("age", -1);
+        //int age = intent.getIntExtra("age", -1);
 
         String message = name + " welcome to your user area";
         welcomeMsg.setText(message);
-        etUsename.setText(username);
-        etAge.setText(age + "");
+        // etUsername.setText(username);
+        // etAge.setText(age + "");
 
     }
 
-    private File getFile() {
-        File folder = new File("SDCARD/camera_app") ;
-        if(!folder.exists()){
-            folder.mkdir();
-        }
-        File image_file = new File(folder, "cam_image.jpg");
-        return image_file ;
-
-    }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) throws NullPointerException {
+        /*
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_GALLERY_REQUEST){
+                Uri imageUri = data.getData();
+
+                InputStream inputStream ;
+
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+                    imageView.setImageBitmap(image);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+*/
+
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+         imageView.setImageBitmap(bitmap);
     }
+
+    public void onImageGalleryClick(View v){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        photoPickerIntent.setDataAndType(data, "image/*");
+
+
+        startActivityForResult(photoPickerIntent,IMAGE_GALLERY_REQUEST );
+    }
+
+
 }
